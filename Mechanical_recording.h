4413ce4652e2ef4771dc77a4edf11c1bfd12738e
Kenton <fedosenkogeorgij44@gmail.com>
2021-02-13 12:44:55 +0300

    Добавлены классы механической записи и флеш записи

---
+++ Mechanical_recording.h
@@ -0,0 +1,14 @@
+#ifndef MEMORY_WRITE_MECHANICAL_RECORDING_H
+#define MEMORY_WRITE_MECHANICAL_RECORDING_H
+
+#include <iostram>
+#include <sttring>
+#include "Carrier.h"
+using namespace std;
+
+class Mechanical_recording {
+    bool laser;
+    bool magnet;
+};
+
+#endif //MEMORY_WRITE_MECHANICAL_RECORDING_H
---
+++ flash_recording.h
@@ -0,0 +1,14 @@
+#ifndef MEMORY_WRITE_FLASH_RECORDING_H
+#define MEMORY_WRITE_FLASH_RECORDING_H
+
+#include <iostram>
+#include <string>
+#include "Carrier.h"
+using namespace std;
+
+class flash_recording {
+    string storageTime;
+    string application;
+};
+
+#endif //MEMORY_WRITE_FLASH_RECORDING_H
